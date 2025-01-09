553da3f402d9f6ecae71975696063318e2d3931e
KoshiBlade <koshisemail@gmail.com>
2025-01-09 15:49:35 +0100

    pyro textures

---
+++ Scripts/skeleton_pyro.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+var health=300
+var damage = 10
+var finished_walk = false
+var can_attack = true
+var freezing = false
+@onready var animation_player = $AnimationPlayer
+
+func _physics_process(_delta):
+	print("walk: ",finished_walk, " progress: ",get_parent_node_3d().progress, " max: ",get_parent_node_3d().curve_length, " hp: ",health)
+	if finished_walk:
+		play_attack_animation()
+		if can_attack:
+			can_attack=false
+			$"Attack CD".start()
+	else:
+		play_run_animation()
+		
+
+func play_attack_animation():
+	if animation_player.current_animation != "Unarmed_Melee_Attack_Punch_A":
+		animation_player.play("Unarmed_Melee_Attack_Punch_A")
+
+
+func play_run_animation():
+	if animation_player.current_animation != "Running_C":
+		animation_player.play("Running_C")
+	
+#Function that changes speed of the enemy when in range of freeze tower
+func change_speed(value:float)->void:
+	get_parent_node_3d().speed=get_parent_node_3d().speed * value
+
+#Function that happens when mob is struck with arrow/spell/canon
+func take_damage(dmg: int) -> void:
+	health-=dmg
+	if (health<=0) and (health+dmg) > 0:
+		get_parent_node_3d().delete_object()
+
+#Resetting attacking 
+func _on_attack_cd_timeout() -> void:
+	take_damage(150)
+	get_parent_node_3d().get_parent_node_3d().get_parent_node_3d().take_damage(damage)
+	can_attack=true
